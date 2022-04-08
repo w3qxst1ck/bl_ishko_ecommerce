2c85bd302102d23f4ea1d3273740e213e29007d3
Alexandr Andreev <aleksandrandreev@MacBook-Pro-Aleksandr.local>
2022-04-08 16:20:30 +0300

    Refactor detail, faq

--- bl_ishko/shop/urls.py
+++ bl_ishko/shop/urls.py
@@ -1,10 +1,12 @@
 
 from django.urls import path
 
-from .views import home_page
+from .views import home_page, faq_page, product_detail
 
 app_name = 'shop'
 
 urlpatterns = [
-    path('', home_page),
+    path('', home_page, name='home-page'),
+    path('faq/', faq_page, name='faq-page'),
+    path('detail/', product_detail, name='detail-page'),
 ]
--- bl_ishko/shop/views.py
+++ bl_ishko/shop/views.py
@@ -2,4 +2,12 @@ from django.shortcuts import render
 
 
 def home_page(request):
-    return render(request, 'shop/index-2.html')
+    return render(request, 'shop/base.html')
+
+
+def faq_page(request):
+    return render(request, 'shop/faq.html')
+
+
+def product_detail(request):
+    return render(request, 'shop/detail.html')
