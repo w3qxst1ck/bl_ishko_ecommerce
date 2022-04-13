855ff65b8c200022087ea3997fbe4ae7294259f1
Alexandr Andreev <aleksandrandreev@MacBook-Pro-Aleksandr.local>
2022-04-13 20:55:13 +0300

    Add page for cart, checkout, wish, registration, login

--- bl_ishko/shop/urls.py
+++ bl_ishko/shop/urls.py
@@ -1,14 +1,23 @@
 
 from django.urls import path
 
-from .views import home_page, faq_page, product_detail, about_page, shop_page
+from .views import (home_page, faq_page, product_detail, about_page,
+                    shop_page, contact_page, cart_page, checkout_page,
+                    login, wishlist_page, registration)
+
 
 app_name = 'shop'
 
 urlpatterns = [
     path('', home_page, name='home-page'),
     path('faq/', faq_page, name='faq-page'),
+    path('cart/', cart_page, name='cart-page'),
+    path('checkout/', checkout_page, name='checkout-page'),
+    path('contact/', contact_page, name='contact-page'),
     path('detail/', product_detail, name='detail-page'),
     path('about/', about_page, name='about-page'),
     path('shop/', shop_page, name='shop-page'),
+    path('wishlist/', wishlist_page, name='wishlist-page'),
+    path('login/', login, name='login'),
+    path('registration/', registration, name='registration'),
 ]
--- bl_ishko/shop/views.py
+++ bl_ishko/shop/views.py
@@ -16,5 +16,30 @@ def product_detail(request):
 def about_page(request):
     return render(request, 'shop/about.html')
 
+
 def shop_page(request):
     return render(request, 'shop/shop.html')
+
+
+def contact_page(request):
+    return render(request, 'shop/contact.html')
+
+
+def cart_page(request):
+    return render(request, 'shop/cart.html')
+
+
+def checkout_page(request):
+    return render(request, 'shop/checkout.html')
+
+
+def login(request):
+    return render(request, 'users/login.html')
+
+
+def wishlist_page(request):
+    return render(request, 'shop/wishlist.html')
+
+
+def registration(request):
+    return render(request, 'users/registration.html')
