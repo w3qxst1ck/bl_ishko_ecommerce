d02312cb67a877f887a479a7346073ceca44f1a0
Alexandr Andreev <aleksandrandreev@MacBook-Pro-Aleksandr.local>
2022-06-15 21:42:27 +0300

    Fix default_from_mail

--- bl_ishko/bl_ishko/settings.py
+++ bl_ishko/bl_ishko/settings.py
@@ -178,7 +178,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-
 # Email settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.getenv('EMAIL_HOST')
@@ -187,6 +186,7 @@ EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 # EMAIL_USE_TLS = bool(int(os.getenv('EMAIL_USE_TLS')))
 EMAIL_USE_SSL = bool(int(os.getenv('EMAIL_USE_SSL')))
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
 
 # Captcha
 RECAPTCHA_PUBLIC_KEY = os.getenv('RECAPTCHA_PUBLIC_KEY')
