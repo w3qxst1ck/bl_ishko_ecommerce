92d7acb8fc701e925571afb5d6f4d2bc76c1428b
Kirill Andreev <hizenberg228@mail.ru>
2022-06-19 19:53:52 +0300

    Add posts for home page

--- bl_ishko/shop/views.py
+++ bl_ishko/shop/views.py
@@ -7,7 +7,7 @@ import os
 from .utils import gen_slug
 from users.models import WishProduct, ProductComment
 from .forms import FormWithCaptcha
-from .models import Product, Category, Faq, FaqCategory
+from .models import Product, Category, Faq, FaqCategory, Post
 from .services import get_related_products_for_detail, get_size_list
 from .tasks import send_messages_from_contact_task
 
@@ -17,7 +17,10 @@ def home_page(request):
         wish_list_products = WishProduct.objects.filter(user=request.user)
     else:
         wish_list_products = []
-    return render(request, 'shop/base.html', context={'wish_list_products': wish_list_products})
+
+    posts = Post.objects.filter(is_active=True)[:2]
+    return render(request, 'shop/base.html', context={'wish_list_products': wish_list_products,
+                                                      'posts': posts,})
 
 
 def faq_page(request, pk=None):
