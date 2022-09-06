84a113872bfae1977229c460f179424e25ee93b4
Kirill Andreev <hizenberg228@mail.ru>
2022-09-06 22:32:11 +0300

    add message to client about paid status

--- bl_ishko/cart/admin.py
+++ bl_ishko/cart/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
+
+
 from .models import Order, OrderItem, BillingInfo
+from .tasks import send_message_to_client_changed_paid_status
+
 
 admin.site.register(OrderItem)
 admin.site.register(BillingInfo)
@@ -9,3 +13,12 @@ admin.site.register(BillingInfo)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ('id', '_get_user_email', 'user', 'created', 'ordered', 'is_active', 'paid',
                     '_get_order_status', 'get_order_total_price_with_sale')
+
+    def save_model(self, request, obj, form, change):
+        if change:
+            if form.initial['paid'] != form.cleaned_data['paid'] and form.cleaned_data['paid']:
+                send_message_to_client_changed_paid_status.delay(obj.id)
+        obj.save()
+
+
+
